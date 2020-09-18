57beaa7aefdef805c6d5203a4bcc471a8644784b
JSemelhago <JSemelhago@users.noreply.github.com>
2020-09-18 16:43:40 -0400

    Fixed formatting

--- src/ProduceText.py
+++ src/ProduceText.py
@@ -3,15 +3,18 @@ import json
 
 df = pd.read_csv('../output/pixel_array.csv')
 
+b = '🅱️'
+space = len(b)
+
 final_output = ''
 
 for index, row in df.iterrows():
     for entry in row:
         entry = json.loads(entry)
         if int(entry[0])>5 or int(entry[1])>5 or int(entry[2])>5:
-            final_output += '🅱️'
+            final_output += b
         else:
-            final_output += '\t'
+            final_output += space*' '
     final_output += '\n'
 
 print(final_output)
