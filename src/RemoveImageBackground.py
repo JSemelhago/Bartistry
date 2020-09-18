3d049d74eb5a972c62ef2ca3b503ce61ad0e51e6
JSemelhago <JSemelhago@users.noreply.github.com>
2020-09-18 11:54:52 -0400

    Generalize image finding

--- src/RemoveImageBackground.py
+++ src/RemoveImageBackground.py
@@ -1,8 +1,11 @@
 import cv2
-import numpy as np
+import glob
+
+#Find image
+img_path = [glob.glob('../data/'+e) for e in ['*.jpg', '*.jpeg', '*.png']][0][0]
 
 #Load image
-img = cv2.imread('../data/banana.jpg')
+img = cv2.imread(img_path)
 
 #Convert to gray scale
 gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -10,19 +13,10 @@ gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Apply Gaussian blur
 blur = cv2.GaussianBlur(gray_scale, (5,5), 0)
 
-#Use Otsu's method to get foreground/background
+#Use Otsu's method to get foreground/background in black and white
 threshold = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-#Bounding rectangle
-x, y, w, h = cv2.boundingRect(threshold)
-
-#Create new rectangle from x y width height (x, y -> x+w, y+h)
-new_base = x+w
-new_height = y+h
-
-cv2.rectangle(img, (x, y), (new_base, new_height), (36, 255, 12), 2)
-
-cv2.imshow('black_white', threshold)
-cv2.waitKey()
+#Output image
+cv2.imwrite('../output/black_white.jpg', threshold)
 
 
