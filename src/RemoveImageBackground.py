d4fdfef8d21f803bc1d1288d4f1e08c2f657e9df
Justin Semelhago <justin.semelhago@mail.utoronto.ca>
2020-09-18 15:30:10 -0400

    Create dataframe from pixellated image

--- src/RemoveImageBackground.py
+++ src/RemoveImageBackground.py
@@ -1,7 +1,6 @@
 import cv2
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
 
 #Find image
 img_path = [glob.glob('../data/'+e) for e in ['*.jpg', '*.jpeg', '*.png']][0][0]
@@ -9,46 +8,33 @@ img_path = [glob.glob('../data/'+e) for e in ['*.jpg', '*.jpeg', '*.png']][0][0]
 #Load image
 img = cv2.imread(img_path)
 
+#Create mask and models for foreground and background
 mask = np.zeros(img.shape[:2], np.uint8)
-
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
 
 img_copy = img.copy()
 
-#Convert to gray scale
+#Convert to and apply gray scale
 gray_scale = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-
 binary = cv2.bitwise_not(gray_scale)
 
+#Get contours from gray scale
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+#Get rectangle coordinates from around contours
 rect = (0,0,0,0)
-
 for contour in contours:
     (x,y,w,h) = cv2.boundingRect(contour)
     rect = (x,y,w,h)
     cv2.rectangle(img_copy, (x,y), (x+w,y+h), (0,255,0), 2)
 
-print(rect)
-
-
+#Grab foreground from rectangle
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)+(mask==0),255,0).astype('uint8')
 img = img*mask2[:,:,np.newaxis]
 
-plt.imshow(img)
-plt.colorbar()
-plt.show()
-
-
-# #Apply Gaussian blur
-# blur = cv2.GaussianBlur(gray_scale, (5,5), 0)
-#
-# #Use Otsu's method to get foreground/background in black and white
-# threshold = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
 #Output image
-cv2.imwrite('../output/black_white.jpg', img)
+cv2.imwrite('../output/black_bkg.jpg', img)
 
 
--- src/ReplaceImage.py
+++ src/ReplaceImage.py
@@ -1,12 +1,19 @@
 from PIL import Image
 import glob
+import numpy as np
+import pandas as pd
 
+#Find jpg and open it
 img_path = glob.glob('../output/*.jpg')[0]
-
 img = Image.open(img_path)
 
-pixel = img.resize((16, 16), resample = Image.BILINEAR)
+#Convert to be 16 pixels by 16 pixels
+result = img.resize((16, 16), Image.ANTIALIAS)
 
-result = pixel.resize(img.size, Image.NEAREST)
+#Convert to dataframe with arrays representing colours
+pixel_array = np.array(result)
+df = pd.DataFrame(pixel_array.tolist())
 
+#Save dataframe and image
+df.to_csv('../output/pixel_array.csv', index=False, header=False)
 result.save('../output/result.png')
